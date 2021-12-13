213c76e79459e511a7590b42f9443e7f1e19f901
alexbrul <42654943+alexbrul@users.noreply.github.com>
2021-12-13 13:02:56 +0200

    added tmp card text

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,9 +6,9 @@ export default function Home() {
     return (
         <div className={classes.container}>
             <div className={classes.gridcontainer}>
-                <CardHome title={"User app"} pButton="Manage Users" description={"Navigate to users app, change and give permissions"} />
-                <CardHome title={"User app"} pButton="Manage Users" description={"Navigate to users app, change and give permissions"} />
-                <CardHome title={"User app"} pButton="Manage Users" description={"Navigate to users app, change and give permissions"} />
+                <CardHome title={"User App"} pButton="Manage Users" description={"Navigate to users app, change and give permissions"} />
+                <CardHome title={"Overall Report"} pButton="Manage Users" description={"Something about generating overall reports"} />
+                <CardHome title={"Report Format"} pButton="Manage Users" description={"Something about changing report formats"} />
             </div>
         </div>
     )
