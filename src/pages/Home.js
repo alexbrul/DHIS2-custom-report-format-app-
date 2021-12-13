49284ff2a64fb318c73779bc54057ee6b4599642
Mikael Rodvelt <mikael-96@hotmail.no>
2021-12-13 16:01:54 +0200

    modal Onclick
    fixed onClick

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 import CardHome from '../components/CardHome'
-import classes from './Home.module.css'
+import PromptModal from '../components/PromptModal'
+import classes from '../styleSheets/home.module.css'
 
 export default function Home() {
+    const [modal, setModal] = useState(false)
+    function handleClick(e){
+        setModal(e)
+    }
     return (
         <div className={classes.container}>
             <div className={classes.gridcontainer}>
-                <CardHome title={"User app"} pButton="Manage Users" description={"Navigate to users app, change and give permissions"} />
-                <CardHome title={"User app"} pButton="Manage Users" description={"Navigate to users app, change and give permissions"} />
-                <CardHome title={"User app"} pButton="Manage Users" description={"Navigate to users app, change and give permissions"} />
+                <CardHome title={"User app"} pButton="Manage" description={"Navigate to users app, change and give permissions"} modalFunction = {handleClick}/>
+                <CardHome title={"Generate Report"} pButton="Build" description={"Build a new report using a configured format"} modalFunction = {handleClick}/>
+                <CardHome title={"Edit Report Format"} pButton="Edit" description={"Edit an existing report format"} modalFunction = {handleClick}/>
             </div>
+            {console.log(modal)}
         </div>
     )
 }
