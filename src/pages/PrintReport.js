6f3a2f16676159ac90893c739c6d43864ee8b88d
alexbrul <42654943+alexbrul@users.noreply.github.com>
2021-12-20 14:33:13 +0200

    Generate report working!

--- src/pages/PrintReport.js
+++ src/pages/PrintReport.js
@@ -7,8 +7,9 @@ import DatasetPreview from '../components/reportBuilder/DatasetPreview'
 const PrintReport = (props) => {
     const { activePage, dataSets } = props
     const { data, refetch, error, loading } = useDataQuery(datasetReportCustomDynamicQuery, {
-        lazy: true,
+        lazy: false,
     })
+    const [dataSetsArray, setDataSetsArray] = useState([])
     const [output, setOutput] = useState("");
 
     const [counter, setCounter] = useState(0);
@@ -20,26 +21,26 @@ const PrintReport = (props) => {
 
     const count = dataSets.length
 
-    const dataSetsArray = []
 
-    useEffect(() => {
-        if (data) {
-            dataSetsArray.push(data.dataSetReport)
-            console.log("inside useEffect triggered by data change")
+    useEffect((props) => {
+        console.log("inside useEffect triggered by data change")
+        if (data && (count>counter)) {
             addCount()
-            console.log("dataset array", dataSetsArray)
+            console.log("if test triggered")
+            console.log("data: ", data)
+            setDataSetsArray([...dataSetsArray, data.dataSetReport])
+            console.log("datasetsarray inside if: ", dataSetsArray)
+            console.log("Data inside useffect: ", data.dataSetReport)
 
         }
-        console.log("counter: ", counter, " count: ", count)
-        if (counter == count && data) {
-            dataSetsArray.map(item => {
-                console.log("item : ", item)
-                document.MyFrame.document.body.innerHTML += item + "-----------------------" + "<br/>" + "<br/>" + "<br/>"
-            })
-        } else {
-            fetchReport(dataSets)
-        }
+
     }, [data])
+
+    useEffect((props) => {
+        fetchReport()
+
+    }, [dataSetsArray])
+
 
 
 
@@ -47,15 +48,21 @@ const PrintReport = (props) => {
     const fetchReport = (args) => {
 
         console.log("Fetching report, count= ", count, " Counter = ", counter)
-
         if (counter < count) {
             refetch({ ds: dataSets[counter], ou: 'nBLRIqKNNOu', pe: '2020' })
-
         }
 
         if (counter == count) {
             console.log("Fetching finished")
+            console.log("datasetsarray finished", dataSetsArray)
             console.log("Dataset library")
+            if ((counter >= count)) {
+                console.log("dataset array", dataSetsArray)
+                dataSetsArray.map(item => {
+                    console.log("item : ", item)
+                    document.MyFrame.document.body.innerHTML += item + "-----------------------" + "<br/>" + "<br/>" + "<br/>"
+                })
+            }
 
         }
     }
@@ -67,10 +74,9 @@ const PrintReport = (props) => {
         <div>
             <Button onClick={() => { activePage('Generate') }}> Back to Builder</Button>
             <h1>This is The Report Printer</h1>
-            <button onClick={() => {
-                { console.log("datasetsbutton: ", dataSets) }
-                fetchReport(dataSets)
-            }}> Build The Report</button>
+            <button onClick={() => 
+                    fetchReport()
+            }> Build The Report</button>
             <DatasetPreview>
                 {(counter==count) && <p>testalex</p>}
                 <p>test</p>
